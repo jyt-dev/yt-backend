38db4131171e5c3e84313c6d81e4a214482103ac
JyotishIJ <anandjyotish4@gmial.com>
2026-05-05 11:59:23 +0530

    comment added for later understanding

--- src/app.js
+++ src/app.js
@@ -1,36 +1,51 @@
-import express, { json } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express, { json } from "express"; //used to create backend server
+import cors from "cors";                 //allows requests from different origins
+import cookieParser from "cookie-parser"; //reads cookies from request
 
 
-const app = express();
+const app = express();  //Initializes your Express application
 
-app.use((req, res, next) => {
+
+// Logs every request , next() → passes control to next middleware
+app.use((req, res, next) => { 
     console.log(`${req.method} request for ${req.url}`);
     next();
 });
 
 
-// configurations
+// configurations 
+// Only allows requests from a specific origin (your frontend)
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({limit: "16kb"}));
-app.use(express.static("public"));
-app.use(cookieParser());
+//body parsing
+app.use(express.json({limit: "16kb"})); // express.json() → parses JSON body and payload limit is 16kb
+app.use(express.urlencoded({limit: "16kb"})); // express.urlencoded() → parses form data 
+
+
+app.use(express.static("public")); // serves static files like image
+app.use(cookieParser()); // parses cookies data
 
 
 
 //routes import
-
+//This file contains all user-related endpoints (e.g., login, register, etc.)
 import userRouter from "./routes/user.routes.js";
 
-//routes declaration
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter); //Route Mounting
+
+// If inside user.routes.js you have:
+// router.get("/login", ...) Final API becomes: /api/v1/users/login
 
 
 
-export {app};
+export {app}; // You export app so it can be used in another file (usually server.js)  : app.listen(PORT)
+
+// Request comes → middleware logs it
+// CORS checks origin
+// Body is parsed
+// Cookies are read
+// Route is matched
+// Controller handles response
