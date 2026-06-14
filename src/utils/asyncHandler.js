1a9fbe9caa402f0f24652e61933576b63c2d5e4c
JyotishIJ <anandjyotish4@gmial.com>
2026-06-14 19:12:16 +0530

    fix(asyncHandler): improve formatting and comments for clarity

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,17 +1,21 @@
 
+
+// asyncHandler is try-catch wrapper that saves from writing try-catch in every async controller
+// it returns a new function (middleware), resolve is like try and catch is used to display error
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((err) => next(err));
     }
 }
-
 export { asyncHandler }
 
 
 
+// try-catch version of above code
 
 /*
-
 const asyncHandler = (fn) => async(req, res, next) => {
 
     try {
