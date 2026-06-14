83854afa85097a0da5be98e1131ccb5c47a0e1ef
JyotishIJ <anandjyotish4@gmial.com>
2026-06-14 19:07:41 +0530

    fix(app): correct express.urlencoded configuration and update video route

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 //body parsing
 app.use(express.json({limit: "16kb"})); // express.json() → parses JSON body and payload limit is 16kb
-app.use(express.urlencoded({limit: "16kb"})); // express.urlencoded() → parses form data 
+app.use(express.urlencoded({extended: true},{limit: "16kb"})); // express.urlencoded() → parses form data 
 
 
 app.use(express.static("public")); // serves static files like image
@@ -34,14 +34,14 @@ app.use(cookieParser()); // parses cookies data
 //This file contains all user-related endpoints (e.g., login, register, etc.)
 import userRouter from "./routes/user.routes.js";
 
-app.use("/api/v1/users", userRouter); //Route Mounting
+app.use("/api/v1/users", userRouter); //Users Route Mounting
 
 // If inside user.routes.js you have:
 // router.get("/login", ...) Final API becomes: /api/v1/users/login
 
 //video routes
 import videoRouter from "./routes/video.routes.js"
-app.use("api/v1/videos", videoRouter);
+app.use("/api/v1/videos", videoRouter); //Video routes mounting
 
 export {app}; // You export app so it can be used in another file (usually server.js)  : app.listen(PORT)
 
